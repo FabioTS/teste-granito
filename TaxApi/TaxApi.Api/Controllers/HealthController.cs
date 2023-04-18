65a753c717bd5971d249fc038a49ab3f1e47de18
FabioTS <fabiots@novaweb.mobi>
2023-04-18 18:52:20 -0300

    health checks

---
+++ TaxApi/TaxApi.Api/Controllers/HealthController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaxApi.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    private readonly ILogger<HealthController> _logger;
+
+    public HealthController(ILogger<HealthController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Health Check
+    /// </summary>
+    [HttpGet]
+    public IActionResult HealthCheck()
+    {
+        _logger.LogInformation(nameof(HealthCheck));
+        return Ok("Alive");
+    }
+}
