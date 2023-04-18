8e548c4ec2558bcaa08a7d9f6223ef8cdcf3615c
FabioTS <fabiots@novaweb.mobi>
2023-04-18 18:28:49 -0300

    more tests

--- CalculatorApi/CalculatorApi.Tests/Handlers/TaxCalculatorHandlerTests.cs
+++ CalculatorApi/CalculatorApi.Tests/Handlers/TaxCalculatorHandlerTests.cs
@@ -17,11 +17,11 @@ public class TaxCalculatorHandlerTests
     }
 
     [TestMethod]
-    [DataRow(1, 1, 1)]
-    [DataRow(50, 10, 1)]
-    [DataRow(300, 24, 1)]
-    [DataRow(124.55, 6, 1)]
-    [DataRow(234.5568, 12, 1)]
+    [DataRow(1, 1, 1.01)]
+    [DataRow(100, 5, 105.10)]
+    [DataRow(300, 24, 380.92)]
+    [DataRow(124.55, 6, 132.21)]
+    [DataRow(234.5568, 12, 264.30)]
     public void ShouldBeOkAndCorrect(double initialValue, int months, double expected)
     {
         var command = BuildCommand(initialValue, months);
@@ -30,8 +30,40 @@ public class TaxCalculatorHandlerTests
         Assert.AreEqual(HttpStatusCode.OK, result.GetStatus());
 
         var json = JsonSerializer.SerializeToDocument(result.GetData());
-        var calculated = json.RootElement.GetProperty("data").GetProperty("result").GetDouble();
+        var calculated = json.RootElement.GetProperty("result").GetDouble();
         Assert.AreEqual(expected, calculated);
+    }
+
+    [TestMethod]
+    [DataRow(1, 1, 1.011)]
+    [DataRow(100, 5, 105.102)]
+    [DataRow(50.5568, 24, 264.30)]
+    public void ShouldBeOkButIncorrect(double initialValue, int months, double expected)
+    {
+        var command = BuildCommand(initialValue, months);
+        var result = _handler.Handle(command).Result;
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.OK, result.GetStatus());
+
+        var json = JsonSerializer.SerializeToDocument(result.GetData());
+        var calculated = json.RootElement.GetProperty("result").GetDouble();
+        Assert.AreNotEqual(expected, calculated);
+    }
+
+    [TestMethod]
+    [DataRow(0, 0)]
+    [DataRow(1, 0)]
+    [DataRow(0, 1)]
+    [DataRow(-100, 1)]
+    [DataRow(100, -1)]
+    [DataRow(-100, -10)]
+    [DataRow(-100.3668, 10)]
+    public void ShouldBeBadRequest(double initialValue, int months)
+    {
+        var command = BuildCommand(initialValue, months);
+        var result = _handler.Handle(command).Result;
+        Assert.IsNotNull(result);
+        Assert.AreEqual(HttpStatusCode.BadRequest, result.GetStatus());
     }
 
     private TaxCalculateCommand BuildCommand(double initialValue, int months)
